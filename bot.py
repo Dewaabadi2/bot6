cf7f4a8cc249fb49043082eb72260aa09a50a6c0
Dewaabadi2 <bsmjaya30@gmail.com>
2026-08-28 14:12:43 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -90,29 +90,14 @@ async def main(nama, email, c):
         #Whatsapp
         await page.mouse.click(370, 770)
         await page.wait_for_timeout(2000)
-        namaGambar = f"{c:03d}.png"
-        await page.screenshot(path=namaGambar)
+        #namaGambar = f"{c:03d}.png"
+        
+        #await page.screenshot(path=namaGambar)
 
         #Live chat
         await page.mouse.click(360, 669)
         await page.wait_for_timeout(2000)
-        #await page.screenshot(path="11.png")
-        #await page.screenshot(path="12.png")
-
-        #aktif chat
-        #await page.wait_for_timeout(3000)
-        #await page.mouse.click(512, 1230)
-        #await page.wait_for_timeout(1000)
-        #await page.screenshot(path="13.png")
-        
-        #Ketik pesan
-        #await page.wait_for_timeout(5000)
-        #await page.keyboard.type("Halo admin")
-        #await page.screenshot(path="14.png")
-        #await page.wait_for_timeout(1000)
-        #await page.keyboard.press("Enter")
-        #await page.wait_for_timeout(3000)
-        #await page.screenshot(path="15.png")
+        print(f"Akun No.{c} {nama} Selesai")
         await browser.close()
 
 if __name__ == "__main__":
