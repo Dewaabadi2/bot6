9dde939434fe8dc783362b6e2d4218b2126a5ece
Dewaabadi2 <bsmjaya30@gmail.com>
2026-08-31 07:54:19 +0700

    Change starting index for contact processing

--- bot.py
+++ bot.py
@@ -102,7 +102,7 @@ async def main(nama, email, c):
 
 if __name__ == "__main__":
     jumlah = 500
-    mulaiDari = 0
+    mulaiDari = 8000
     for i in range (mulaiDari, mulaiDari+jumlah):
         contact = contacts[i]        
         nama = contact["nama"]
