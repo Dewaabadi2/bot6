c3852e73c4da9ac790e88b9596121aa1c8604ff8
Dewaabadi2 <bsmjaya30@gmail.com>
2026-09-05 23:03:26 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,14 +63,16 @@ async def main(nama, email, c):
         #await page.screenshot(path=f"{c}_4hall.png")
         # 7. Filter booth
         await page.mouse.click(420, 30)
-        await page.wait_for_timeout(500)
+        await page.wait_for_timeout(1000)
         #await page.screenshot(path=f"{c}_5filter.png")
-        await page.keyboard.type("imi")
-        await page.wait_for_timeout(500)
+        await page.keyboard.type("imigra")
+        await page.wait_for_timeout(1000)
         #await page.screenshot(path=f"{c}_6booth.png")
         await page.keyboard.press("Enter")
-        await page.wait_for_timeout(500)
+        await page.wait_for_timeout(1000)
         await page.mouse.click(420, 30)
+        await page.keyboard.type("imigra")
+        await page.wait_for_timeout(1000)
         await page.keyboard.press("Enter")
         # Sudah masuk boot
 
@@ -79,12 +81,12 @@ async def main(nama, email, c):
         await page.mouse.click(357, 624) #video
         #await page.mouse.click(170, 662) #poster kiri
         #await page.mouse.click(540, 662) #poster kanan
-
-        if(c==0):
-            await page.wait_for_timeout(2000)
+        await page.wait_for_timeout(2000)
+        if(c==0):            
             await page.screenshot(path=f"{c}_9last.png")
 
         print(f"Akun : ({c}) {nama} | Selesai")
+        await page.wait_for_timeout(2000)
         await browser.close()
 
 if __name__ == "__main__":
